7d72ce4739275bb1f602c16de65a88290c896d4b
Chen ZHANG <chen.zhang_06sept@foxmail.com>
2025-08-10 01:18:44 +0800

    :bricks: doxygen build args

--- nt.ps1
+++ nt.ps1
@@ -1,5 +1,5 @@
 conan create . -pr:b=default -pr:h=default -s build_type=Debug --build=missing
-doxygen Doxyfile
+python .\docs\build.py
 Remove-Item -Path .\test_package\build -Recurse -Force
 Remove-Item -Path .\test_package\CMakeUserPresets.json
 conan remove "fcpp/*" --confirm
